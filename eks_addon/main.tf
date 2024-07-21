fcf4614fe0e509ea4dc6e56139fdaf86d93ad2e8
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-20 18:35:41 -0600

    adding addons

--- eks_addon/main.tf
+++ eks_addon/main.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_addon" "coredns" {
+resource "aws_eks_addon" "this" {
   cluster_name  = var.cluster_name
   addon_name    = var.addon_name
   addon_version = var.addon_version # Adjust based on EKS version 1.30 compatibility
