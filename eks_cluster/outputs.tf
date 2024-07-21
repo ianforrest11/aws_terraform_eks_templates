2883e90efb8b09e98f22506a00e833b7c5bb5d00
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-20 19:00:17 -0600

    adding cluster security group id output

--- eks_cluster/outputs.tf
+++ eks_cluster/outputs.tf
@@ -3,6 +3,11 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.this.endpoint
 }
 
+output "cluster_security_group_id" {
+  description = "The security group ID created by EKS for the cluster."
+  value       = aws_eks_cluster.this.cluster_security_group_id
+}
+
 output "cluster_certificate_authority_data" {
   description = "The certificate data for the EKS control plane."
   value       = aws_eks_cluster.this.certificate_authority.0.data
