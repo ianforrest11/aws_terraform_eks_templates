058d44eaf697dd019b760ed1c534040d7f73863b
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-21 11:40:35 -0600

    adding endpoint access parameters

--- eks_cluster/kubernetes_config_map.tf
+++ eks_cluster/kubernetes_config_map.tf
@@ -1,10 +1,3 @@
-### Create a new file for the Kubernetes ConfigMap
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
 resource "kubernetes_config_map" "aws_auth" {
   depends_on = [aws_eks_cluster.this]
 
