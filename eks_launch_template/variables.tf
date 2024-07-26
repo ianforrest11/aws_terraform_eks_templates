22bfb72b4e96b7468955e757005408b0c1083f68
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-26 11:56:34 -0600

    save progress of node group template & launch template therein

--- eks_launch_template/variables.tf
+++ eks_launch_template/variables.tf
@@ -70,6 +70,12 @@ variable "user_data" {
   type        = string
 }
 
+variable "market_type" {
+  description = "set underlying ec2 configs to use spot instances"
+  nullable    = false
+  type        = string
+}
+
 variable "k8s_cluster_dns_ip" {
   description = "The DNS IP address of the Kubernetes cluster"
   type        = string
