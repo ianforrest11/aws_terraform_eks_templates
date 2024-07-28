e78e608d04f7de31f2ed7971af7eecef0728ee3e
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-28 14:32:52 -0600

    separating out autoscaling group tagger from node group

--- eks_autoscaling_group_tag/main.tf
+++ eks_autoscaling_group_tag/main.tf
@@ -1,16 +1,10 @@
 #EKS can't directly set the "Name" tag, so we use the autoscaling_group_tag resource. 
 resource "aws_autoscaling_group_tag" "this" {
-  for_each = toset(
-    [for asg in flatten(
-      [for resources in var.node_group_name : resources.autoscaling_groups]
-    ) : asg.name]
-  )
-
-  autoscaling_group_name = each.value
+  autoscaling_group_name = var.node_group_name[0].autoscaling_group
 
   tag {
-    key   = "Name"
-    value = "${var.node_group_name}_instance"
+    key                 = "Name"
+    value               = "${var.node_group_name[0].node_group_name}_instance"
     propagate_at_launch = true
   }
 }
